6aca4283dc2307c3cdd0334e02a64b404647b2da
Gavin Wong <gavinmwong@gmail.com>
2020-06-22 16:08:36 -0700

    cnn

---
+++ cnn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 14:58:58 2020
+
+@author: gavinwong
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
