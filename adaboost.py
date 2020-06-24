1b96d2e3de51450a9ddd796eba38005489332c57
Gavin Wong <gavinmwong@gmail.com>
2020-06-24 13:49:55 -0700

    finished adaboost

--- adaboost.py
+++ adaboost.py
@@ -5,9 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('')
-X = dataset.iloc[:, :].values
-Y = dataset.iloc[:, :].values
+dataset = pd.read_csv('all_data.csv')
+X = dataset.iloc[:, 33:].values
+Y = dataset.iloc[:, 0].values
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
@@ -21,9 +26,9 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, rand
 
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
-classifier = AdaBoostClassifier(base_estimator = DecisionTreeClassifier(max_depth = 1), n_estimators = 25, learning_rate = 1) ##Might need to change n_estimators
+classifier = AdaBoostClassifier(n_estimators = 500, learning_rate = 1) ##Might need to change n_estimators
 classifier.fit(X_train, Y_train)
 
 
-classifier.score(X_train, Y_train)
-classifier.score(X_test, Y_test)
+print(classifier.score(X_train, Y_train))
+print(classifier.score(X_test, Y_test))
