5cbaabdea37e5ecea1398d73394ba84761935676
Gavin <gavinmwong@gmail.com>
2020-06-26 16:40:23 -0700

    random forest

--- randomforest.py
+++ randomforest.py
@@ -7,31 +7,45 @@ import seaborn as sn
 
 
 dataset = pd.read_csv('all_data.csv')
-X = dataset.iloc[:, 33:].values
-Y = dataset.iloc[:, 0].values
-print(X)
-print(Y)
+X = dataset.iloc[:, 34:]
+Y = dataset.iloc[:, 1]
 
-# from sklearn.preprocessing import LabelEncoder
-# le = LabelEncoder()
-# Y = le.fit_transform(Y)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+Y = le.fit_transform(Y)
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 42)
 
 
 
-# Not needed for Random Forest
-# Feature Scaling
-# from sklearn.preprocessing import StandardScaler
-# sc = StandardScaler()
-# X_train = sc.fit_transform(X_train)
-# X_test = sc.transform(X_test)
+#n_estimators = [500, 800, 1500, 2500, 5000]
+n_estimators = [300]
+min_samples_split = [2, 5, 10, 15, 20]
+min_samples_leaf = [1, 2, 5, 10, 15]
+max_features = ['auto', 'sqrt','log2']
+#max_depth = [10, 20, 30, 40, 50, 60, 70, 80]
+max_depth = [30, 40, 50, 60, 70, 80]
+max_depth.append(None)
+
+
+
+
+grid_param = {'n_estimators': n_estimators, 'max_features': max_features, 'max_depth':max_depth, 'min_samples_split':min_samples_split, 'min_samples_leaf': min_samples_leaf}
 
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 50, criterion = 'entropy', random_state = 0) #Might need to change n_estimators
+from sklearn.model_selection import RandomizedSearchCV
+classifier = RandomForestClassifier(random_state = 0)
+RFC_random = RandomizedSearchCV(estimator = classifier, param_distributions = grid_param, n_iter = 500, verbose = 2, random_state = 42, n_jobs = -1)
+RFC_random.fit(X, Y)
+print(RFC_random.best_score_)
+print(RFC_random.best_params_)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 300, min_samples_split = 2, min_samples_leaf = 2, max_features = 'sqrt', max_depth = 30)
 classifier.fit(X_train, Y_train)
 Y_pred = classifier.predict(X_test)
+
 
 
 #Confusion Matrix
@@ -47,33 +61,28 @@ from sklearn.metrics import accuracy_score
 accuracy_score(Y_train, classifier.predict(X_train))
 accuracy_score(Y_test, Y_pred)
 
-#Precision and Recall
+#Precision, Recall, F1Score
 from sklearn.metrics import precision_recall_fscore_support
 precision_recall_fscore_support(Y_test, Y_pred, average ='micro')
 precision_recall_fscore_support(Y_test, Y_pred, average ='macro')
 
-def plot_graphs(history, best):
 
-  plt.figure(figsize=[10,4])
-  # summarize history for accuracy
-  plt.subplot(121)
-  #plt.plot(history.history['acc'])
-  plt.plot(history.history['val_acc'])
-  plt.title('model accuracy across training\n best accuracy of %.02f'%best[1])
-  plt.ylabel('accuracy')
-  plt.xlabel('epoch')
-  plt.legend(['train', 'test'], loc='upper left')
 
-  # summarize history for loss
-  plt.subplot(122)
-  plt.plot(history.history['loss'])
-  plt.plot(history.history['val_loss'])
-  plt.title('model loss across training\n best loss of %.02f'%best[0])
-  plt.ylabel('loss')
-  plt.xlabel('epoch')
-  plt.legend(['train', 'test'], loc='upper left')
-  plt.show()
+##RESUTS
+#n estimators: 100
+  #{'n_estimators': 100, 'min_samples_split': 2, 'min_samples_leaf': 1, 'max_features': 'auto', 'max_depth': 50}
+  #3 hours
+#n estimators: 150
+  #
 
+##Results
+# {'n_estimators': 200, 'min_samples_split': 2, 'min_samples_leaf': 2, 'max_features': 'auto', 'max_depth': 50}
+#20 minutes
+#0.9943703703703703 training
+#0.9337777777777778 testing
+
+##Results
+#{'n_estimators': 300, 'min_samples_split': 2, 'min_samples_leaf': 2, 'max_features': 'sqrt', 'max_depth': 30}
 
 
 
