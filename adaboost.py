61e8848f761cab62e059ae420509d28e5f72a6cd
Gavin <gavinmwong@gmail.com>
2020-06-29 16:17:23 -0700

    adaboost optimizer

--- adaboost.py
+++ adaboost.py
@@ -18,23 +18,19 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
 
-#n_estimators = [500, 800, 1500, 2500, 5000]
+#n_estimators = [100, 200, 300, 500, 800, 1000]
 n_estimators = [500]
-min_samples_split = [2, 5, 10, 15, 20]
-min_samples_leaf = [1, 2, 5, 10, 15]
-max_features = ['auto', 'sqrt','log2']
-#max_depth = [10, 20, 30, 40, 50, 60, 70, 80]
-max_depth = [30, 40, 50, 60, 70, 80]
-max_depth.append(None)
+learning_rate = [0.5, 1, 1.5, 2]
+algorithm = ['SAMME', 'SAMME.R']
 
 
 
 
-grid_param = {'n_estimators': n_estimators, 'max_features': max_features, 'max_depth':max_depth, 'min_samples_split':min_samples_split, 'min_samples_leaf': min_samples_leaf}
+grid_param = {'n_estimators': n_estimators, 'learning_rate': learning_rate, 'algorithm' : algorithm}
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import RandomizedSearchCV
-classifier = RandomForestClassifier(random_state = 0)
+classifier = AdaBoostClassifier()
 RFC_random = RandomizedSearchCV(estimator = classifier, param_distributions = grid_param, n_iter = 500, verbose = 2, random_state = 42, n_jobs = -1)
 RFC_random.fit(X, Y)
 print(RFC_random.best_score_)
