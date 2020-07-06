4065806ab928fccf823acfb9df8369708416c078
Gavin <gavinmwong@gmail.com>
2020-07-06 15:26:53 -0700

    hmm

--- hmm.py
+++ hmm.py
@@ -13,25 +13,37 @@ import tensorflow as tf
 #import matplotlib.pyplot as plt
 import re
 
-dataset = pd.read_csv('all_data2_new.csv')
+dataset = pd.read_csv('/kaggle/input/all_data2_new.csv')
 X_train = dataset.iloc[1:131, 34:].values
 X_test = dataset.iloc[133:134, 34:].values
+X_test2 = dataset.iloc[1000:1001, 34:].values
+print(X_test.shape)
+test = np.array([0,5,1])
+print(test.shape)
 
+mymodel = hmm.MultinomialHMM(n_components=5, n_iter=500, tol=0.5)  
+mymodel.fit(X_train)
 
-
-mymodel = train_hmm_random_restarts(X_train)
-Y = mymodel.score(X_test)
+Y1 = mymodel.score(X_test)
+Y2 = mymodel.score(X_test2)
 print(Y)
-random_restarts = 10
+print(Y2)
+
+#in CSV ADLOAD is from row 1 to 162
+from statistics import mean
+log_prob_true = []
+for i in range(131, 163):
+    data = dataset.iloc[i:i+1, 34:].values
+    y = mymodel.score(data)
+    print(y)
+    log_prob_true.append(y)
 
 
 def train_hmm_random_restarts(obs_seq):
-	random_restarts = 50
-	model = hmm.MultinomialHMM(n_components=5, n_iter=500, tol=0.5)
-	model.fit(X=obs_seq)
-	prev_model = model
-	return prev_model
-        # model.verbose=True
+    random_restarts = 50
+    model= hmm.MultinomialHMM(n_components=5, n_iter=500, tol=0.5)     # model.verbose=True
+    model.fit(X=obs_seq)
+    prev_model = model
 #     prev_log_prob = model.monitor_.history.pop()
 #     #random_restarts = 0
 #     while(random_restarts!=0):
@@ -43,6 +55,7 @@ def train_hmm_random_restarts(obs_seq):
 #             prev_model = model
 #             prev_log_prob = log_prob
 #         random_restarts -= 1
+    return prev_model
 
 
 
