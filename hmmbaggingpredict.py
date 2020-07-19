c232655eee3ef3f257d74e51018b45a2db34e764
Gavin Wong <gavinmwong@gmail.com>
2020-07-19 11:16:11 -0700

    bagging predict

--- hmmbaggingpredict.py
+++ hmmbaggingpredict.py
@@ -1,9 +1,12 @@
 import pickle
 import numpy as np
 
+
+
+
 families = [ 'ADLOAD', 'AGENT' , 'ALLAPLE_A', 'BHO', 'BIFROSE', 'CEEINJECT', 'CYCBOT_G','FAKEREAN', 'HOTBAR', 'INJECTOR',
-            'ONLINEGAMES', 'RENOS', 'RIMECUD_A', 'SMALL', 'TOGA_RFN', 'VB', 'VBINJECT',
-            'VOBFUS', 'VUNDO', 'WINWEBSEC', 'ZBOT']
+        'ONLINEGAMES', 'RENOS', 'RIMECUD_A', 'SMALL', 'TOGA_RFN', 'VB', 'VBINJECT',
+        'VOBFUS', 'VUNDO', 'WINWEBSEC', 'ZBOT']
 
 file = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Bagging/finalized_model'
 fileX = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Bagging/X_test.sav'
@@ -12,43 +15,45 @@ fileY = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Baggin
 X_test = pickle.load(open(fileX, 'rb'))
 Y_test = pickle.load(open(fileY, 'rb'))
 Y_pred = np.empty(0, dtype=np.int8)
-for i in range(1,6): #change later
-    for row in X_test:
-        array = [0] * 21
-        modelFile = file + str(i) + '.sav'
-        all_models = pickle.load(open(modelFile, 'rb'))
-        pred = predict(row, all_models)
-        array[pred] += 1
-    final_Pred = findMax(array)
-    global Y_pred
-    Y_pred = np.append(Y_pred, final_Pred)
-    print(families[final_Pred])
-        
+scores = [0] * 21
 
-
-def predict(row, all_models): #data is 2d np array
+def predict(row, modelFile): #data is 2d np array
+    all_models = pickle.load(open(modelFile, 'rb'))
     bestScore = -9999999999
-    best_model = 0
     count = -1
     for model in all_models:
         count += 1
         try:
             score = model.score(np.reshape(row, (-1, 1)))
-            if score > bestScore:
-                bestScore = score
-                best_model = count
+            global scores
+            scores[count] += score
         except:
             continue
-    return best_model
 
-def findMax(array):
-    bestScore = -1
+def checkPred(array):
+    bestScore = -9999999999
     best_model = -1
-    for i in range(20):
-        if (array[i] > bestScore):
+    for i in range(21):
+        if array[i] > bestScore:
             bestScore = array[i]
             best_model = i
     return best_model
+
+
+
+for row in X_test: #change later
+    for i in range(1,6):
+        modelFile = file + str(i) + '.sav'
+        predict(row, modelFile)
+    final_Pred = checkPred(scores)
+    Y_pred = np.append(Y_pred, final_Pred)
+    print(families[final_Pred])
+    scores = [0] * 21
+
+        
+
+
+
         
     
     
