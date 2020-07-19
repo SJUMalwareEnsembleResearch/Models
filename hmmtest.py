5ec19cede618679ca321f0793d8b2df350251ee0
Gavin Wong <gavinmwong@gmail.com>
2020-07-19 11:16:28 -0700

    better hmm model

--- hmmtest.py
+++ hmmtest.py
@@ -1,41 +1,64 @@
-from hmmlearn import hmm
+import pickle
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sn
 
-dataset = pd.read_csv('all_data2_new.csv')
-X = dataset.iloc[0:130, 34:].values
-Y = dataset.iloc[0:130, 1].values
 
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-Y = le.fit_transform(Y)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 42)
-print(X_test.shape)
 
-Y_pred = np.empty(0)
-def predict(data): #data is 2d np array
-    for row in data:
-        max_prob = -9999999999
-        best_model = 0
-        count=0
-        for model in all_models:
-            score = model.score(np.reshape(row, (-1, 2)))
-            if score > max_prob:
+families = [ 'ADLOAD', 'AGENT' , 'ALLAPLE_A', 'BHO', 'BIFROSE', 'CEEINJECT', 'CYCBOT_G','FAKEREAN', 'HOTBAR', 'INJECTOR',
+        'ONLINEGAMES', 'RENOS', 'RIMECUD_A', 'SMALL', 'TOGA_RFN', 'VB', 'VBINJECT',
+        'VOBFUS', 'VUNDO', 'WINWEBSEC', 'ZBOT']
+
+file = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Bagging/finalized_model'
+fileX = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Bagging/X_test.sav'
+fileY = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Bagging/Y_test.sav'
+
+X_test = pickle.load(open(fileX, 'rb'))
+Y_test = pickle.load(open(fileY, 'rb'))
+Y_pred = np.empty(0, dtype=np.int8)
+scores = [0] * 21
+array = [0] * 21
+
+def predict(row, modelFile): #data is 2d np array
+    all_models = pickle.load(open(modelFile, 'rb'))
+    bestScore = -9999999999
+    best_model = 0
+    count = -1
+    for model in all_models:
+        count += 1
+        try:
+            score = model.score(np.reshape(row, (-1, 1)))
+            if score > bestScore:
+                bestScore = score
                 best_model = count
-            count += 1
-        Y_pred = np.append(Y_pred, count)
-        print(families[count])
+        except:
+            continue
+    return best_model
 
-# Displaying results
-model = hmm.MultinomialHMM(n_components=5, n_iter=100, tol=0.5)
-model.fit(X_train)
-print("done training")
-for row in X_test:
-    row = np.reshape(row, (-1, 1))
-    print(model.score(row))
+def checkPred(array):
+    bestScore = -1
+    best_model = -1
+    for i in range(21):
+        if array[i] > bestScore:
+            bestScore = array[i]
+            best_model = i
+    return best_model
 
 
+
+for row in X_test: #change later
+    for i in range(1,6):
+        modelFile = file + str(i) + '.sav'
+        pred = predict(row, modelFile)
+        array[pred] += 1
+    final_Pred = checkPred(array)
+    Y_pred = np.append(Y_pred, final_Pred)
+    print(families[final_Pred])
+    array = [0] * 21
+
+filename4 = '/Users/gavinwong/Desktop/Repos/SJUMalwareEnsembleResearch/Models/Bagging/Y_pred.sav'
+# print(Y_pred)
+pickle.dump(Y_pred, open(filename4, 'wb'))
+
+from sklearn.metrics import accuracy_score
+print("-------------------------")
+print(accuracy_score(Y_test, Y_pred))
