e7ced62abf907a08c96afdfa2585adcc4bb797c1
Gavin <gavinmwong@gmail.com>
2020-07-23 20:20:43 -0700

    metric

--- metric_template.py
+++ metric_template.py
@@ -12,30 +12,59 @@ import seaborn as sns
 
 
 #Confusion Matrix #2
-from sklearn.metrics import confusion_matrix
+from collections import Counter
+from sklearn import metrics
+mapping = Counter(Y_pred)
+#print(Counter(y_test))
+mapping = dict(sorted(mapping.items()))
+#--- 259.12324500083923 seconds ---
+
 label_map = {"0":"ADLOAD","1":"AGENT","2":"ALLAPLE_A","3":"BHO","4":"BIFROSE","5":"CEEINJECT","6":"CYCBOT_G","7":"FAKEREAN","8":"HOTBAR","9":"INJECTOR","10":"ONLINEGAMES","11":"RENOS","12":"RIMECUD_A","13":"SMALL","14":"TOGA_RFN","15":"VB","16":"VBINJECT","17":"VOBFUS", "18":"VUNDO","19":"WINWEBSEC","20":"ZBOT"  }
+
+#print(y_test)
+
 def write_cm(cm):
-    file = open("/Users/allen/Desktop/Malware-Research/confusion_matrix_files/cm_xgboost.txt","w")
+    file = open("D:\\Repos\\SJUMalwareEnsembleResearch\\Models\\cm\\cm_boosthmm.txt","w")
     for y in range(0, 21):
         for x in range(0, 21):
-            string = (str(x) + " " + str(y) + " "+ str(round(cm[x][y],4)))
+            string = (str(x) + " " + str(y) + " "+ str(round(cm[y][x],4)))
             file.write(string + "\n")
     file.close()
 
 def plot_confusion_matrix(y_true,y_predicted):
-    cm = confusion_matrix(Y_test, Y_pred)
-    cm = list((cm.T / cm.astype(np.float).sum(axis=1)).T)
-    for y in range(0, 21):
-        for x in range(0, 21):
-            cm[x][y] = round(cm[x][y], 2)
-    write_cm(cm)
-    print ("Plotting the Confusion Matrix")
-    labels = list(label_map.values())
-    df_cm = pd.DataFrame(cm,index = labels,columns = labels)
-    fig = plt.figure()
-    res = sns.heatmap(df_cm, annot=True,cmap='Blues', fmt='g')
+    cm = metrics.confusion_matrix(y_true, y_predicted)
+    l = list(cm)
+    #print(l)
+    s = 0
+    for array in l:
+        for value in array:
+            s += value
+    ooga = []
+    counter = 0
+    for array in l:
+        array = list(array)
+        array = [round(x /mapping[counter],3)  for x in array]
+        ooga.append(array)
+        counter += 1
 
-    plt.yticks([0.5,1.5,2.5,3.5,4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, 21.5], labels,va='center')
+    print(ooga)
+
+
+    #cm = list((cm.T / cm.astype(np.float).sum(axis=1)).T)
+
+
+    write_cm(ooga)
+    #print ("Plotting the Confusion Matrix")
+
+
+    labels = list(label_map.values())
+
+
+    df_cm = pd.DataFrame(ooga,index = labels,columns = labels)
+    fig = plt.figure(figsize=(20,10))
+    ax = sns.heatmap(df_cm, annot=True,cmap='Blues', fmt='g')
+
+    plt.yticks([0.5,1.5,2.5,3.5,4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5], labels,va='center')
 
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
@@ -43,18 +72,8 @@ def plot_confusion_matrix(y_true,y_predicted):
     plt.show()
     plt.close()
 
-plot_confusion_matrix(y_test, best_preds)
+plot_confusion_matrix(Y_test, Y_pred)
 
-#Confusion Matrix
-from sklearn.metrics import confusion_matrix
-families = [ 'ADLOAD', 'AGENT' , 'ALLAPLE_A', 'BHO', 'BIFROSE', 'CEEINJECT', 'CYCBOT_G','FAKEREAN', 'HOTBAR', 'INJECTOR',
-            'ONLINEGAMES', 'RENOS', 'RIMECUD_A', 'SMALL', 'TOGA_RFN', 'VB', 'VBINJECT',
-            'VOBFUS', 'VUNDO', 'WINWEBSEC', 'ZBOT']
-cm = confusion_matrix(Y_test, Y_pred)
-df_cm = pd.DataFrame(cm, index = [i for i in families],
-                  columns = [i for i in families])
-plt.figure(figsize = (10,7))
-sn.heatmap(df_cm, annot=True)
 
 #Overall Accuracy
 from sklearn.metrics import accuracy_score
