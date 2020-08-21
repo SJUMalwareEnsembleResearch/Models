fdc199058e8bf348c58ed398f89d1dcb6baab550
Allen Ye <46653284+allenye66@users.noreply.github.com>
2020-08-20 21:52:48 -0700

    Delete gay.py

--- gay.py
+++
@@ -1,7 +0,0 @@
-import pickle
-import numpy as np
-filename = 'finalized_model.sav'
-
-all_models = pickle.load(open(filename, 'rb'))
-model = all_models[20]
-print(model.score(np.reshape([1, 3, 3, 7, 3, 2, 10, 20, 29], (-1, 1))))
